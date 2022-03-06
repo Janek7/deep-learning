46f40a18cba7b288d96eedfec90846151bd239c1
Janek Putz <janekputz@web.de>
2022-03-06 18:48:58 +0100

    lab2: add sgd_manual_old.py

---
+++ 02_training_nn/sgd_manual_old.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# TEAM MEMBERS:
+# Antonio Krizmanic - 2b193238-8e3c-11ec-986f-f39926f24a9c
+# Jan Kilic - 079f5b80-9807-11ec-986f-f39926f24a9c
+# Janek Putz - e31a3cae-8e6c-11ec-986f-f39926f24a9c
+
+import argparse
+import datetime
+import os
+import re
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") # Report only TF errors by default
+
+import numpy as np
+import tensorflow as tf
+
+from mnist import MNIST
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
+parser.add_argument("--hidden_layer", default=100, type=int, help="Size of the hidden layer.")
+parser.add_argument("--learning_rate", default=0.1, type=float, help="Learning rate.")
+parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument("--seed", default=42, type=int, help="Random seed.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+# If you add more arguments, ReCodEx will keep them with your default values.
+
+class Model(tf.Module):
+    def __init__(self, args: argparse.Namespace) -> None:
+        self._args = args
+
+        self._W1 = tf.Variable(tf.random.normal([MNIST.W * MNIST.H * MNIST.C, args.hidden_layer], stddev=0.1, seed=args.seed), trainable=True)
+        self._b1 = tf.Variable(tf.zeros([args.hidden_layer]), trainable=True)
+
+        # (sgd_backpropagation): Create variables:
+        # - _W2, which is a trainable Variable of size [args.hidden_layer, MNIST.LABELS],
+        #   initialized to `tf.random.normal` value with stddev=0.1 and seed=args.seed,
+        self._W2 = tf.Variable(tf.random.normal([args.hidden_layer, MNIST.LABELS], stddev=0.1, seed=args.seed),
+                               trainable=True)
+        # - _b2, which is a trainable Variable of size [MNIST.LABELS] initialized to zeros
+        self._b2 = tf.Variable(tf.zeros([MNIST.LABELS]), trainable=True)
+
+    def predict(self, inputs: tf.Tensor) -> tf.Tensor:
+        # (sgd_backpropagation): Define the computation of the network. Notably:
+        # - start by reshaping the inputs to shape [inputs.shape[0], -1].
+        #   The -1 is a wildcard which is computed so that the number
+        #   of elements before and after the reshape fits.
+        inputs = tf.reshape(inputs, shape=[inputs.shape[0], -1])
+        # - then multiply the inputs by `self._W1` and then add `self._b1`
+        hidden = inputs @ self._W1 + self._b1
+        # - apply `tf.nn.tanh`
+        hidden = tf.nn.tanh(hidden)
+        # - multiply the result by `self._W2` and then add `self._b2`
+        hidden2 = hidden @ self._W2 + self._b2
+        # - finally apply `tf.nn.softmax` and return the result
+        output = tf.nn.softmax(hidden2)
+
+        # : In order to support manual gradient computation, you should
+        # return not only the output layer, but also the hidden layer after applying
+        # tf.nn.tanh, and the input layer after reshaping.
+        return inputs, hidden, output
+
+    def train_epoch(self, dataset: MNIST.Dataset) -> None:
+        for i, batch in enumerate(dataset.batches(self._args.batch_size)):
+            # The batch contains
+            # - batch["images"] with shape [?, MNIST.H, MNIST.W, MNIST.C]
+            # - batch["labels"] with shape [?]
+            # Size of the batch is `self._args.batch_size`, except for the last, which
+            # might be smaller.
+
+            # : Contrary to sgd_backpropagation, the goal here is to compute
+            # the gradient manually, without tf.GradientTape. ReCodEx checks
+            # that `tf.GradientTape` is not used and if it is, your solution does
+            # not pass.
+
+            # : Compute the input layer, hidden layer and output layer
+            # of the batch images using `self.predict`.
+            input, hidden, output = self.predict(batch["images"])
+
+            # TODO: Compute the gradient of the loss with respect to all
+            # variables. Note that the loss is computed as:
+            # - for every batch example, it is the categorical crossentropy of the
+            #   predicted probabilities and gold batch label
+            # - finally, the individual batch example losses are averaged
+            #
+            # During the gradient computation, you will need to compute
+            # a so-called outer product
+            #   `C[a, i, j] = A[a, i] * B[a, j]`
+            # which you can for example as
+            #   `A[:, :, tf.newaxis] * B[:, tf.newaxis, :]`
+            # or with
+            #   `tf.einsum("ai,aj->aij", A, B)`
+
+            #print(f"{i + 1}. Batch".center(50, '-'))
+            if batch["labels"].shape != (self._args.batch_size, 10):
+                batch["labels"] = tf.keras.utils.to_categorical(batch["labels"])
+                if batch["labels"].shape != (self._args.batch_size, 10):
+                    print("ERROR")
+                    continue
+                # print(batch["labels"])
+            # loss = tf.losses.CategoricalCrossentropy(reduction='none')(batch["labels"], output)
+
+            # 1) W2, B2 -> output layer
+
+            # COMPUTE DERIVATIVES
+            # print(f"Shape Output: {output.shape}, Shape Labels: {batch['labels'].shape}")
+            # compute derivative of loss and y_in (logits of output without softmax application)
+            derivative_loss_yin = tf.math.subtract(output, batch["labels"])
+            derivative_loss_yin_averaged = tf.reduce_mean(derivative_loss_yin, axis=0)
+            #print(derivative_loss_yin)
+            # print(f"Shape of derivative of loss w.r.t output logits ('blue circle'): {derivative_loss_yin.shape}")
+
+            # COMPUTE GRADIENTS
+            # gradient computation of W2
+            derivative_loss_w2 = tf.einsum("ai,aj->aij", hidden, derivative_loss_yin)
+
+            # print(f"Shape outer product: {hidden.shape} -> hidden * {derivative_loss_yin.shape} -> loss derivative = {derivative_loss_w2.shape}")
+            # average gradients over batches -> reduce from (batchsize, 100, 10) to (100, 10)
+            gradients_w2 = tf.reduce_mean(derivative_loss_w2, axis=0)
+            # print(f"Shape of averaged gradients: {gradients.shape}")
+
+            # 1) W1, B1 -> hidden layer
+            derivative_hidden_activation_function = 1 - tf.math.square(hidden)
+            s2 = (derivative_loss_yin @ tf.transpose(self._W2)) @ derivative_hidden_activation_function
+            s2_averaged = tf.reduce_mean(s2, axis=0)
+            gradients_w1 = tf.einsum("ai,aj->aij", input, s2_averaged)
+
+            # TODO(sgd_backpropagation): Perform the SGD update with learning rate `self._args.learning_rate`
+            # for the variable and computed gradient. You can modify
+            # variable value with `variable.assign` or in this case the more
+            # efficient `variable.assign_sub`.
+            # print(f"Shape of W2: {self._W2.shape}")
+            self._W2.assign_sub(self._args.learning_rate * gradients_w2)
+            self._b2.assign_sub(self._args.learning_rate * derivative_loss_yin_averaged)
+            self._W1.assign_sub(self._args.learning_rate * gradients_w1)
+            self._b1.assign_sub(self._args.learning_rate * s2_averaged)
+
+    def evaluate(self, dataset: MNIST.Dataset) -> float:
+        # Compute the accuracy of the model prediction
+        correct = 0
+        for batch in dataset.batches(self._args.batch_size):
+            # : Compute the probabilities of the batch images
+            input, hidden, probabilities = self.predict(batch["images"])
+
+            # : Evaluate how many batch examples were predicted
+            # correctly and increase `correct` variable accordingly.
+            for i in (tf.math.argmax(probabilities, 1).numpy() == batch["labels"]):
+                if i:
+                    correct += 1
+
+        return correct / dataset.size
+
+
+def main(args: argparse.Namespace) -> float:
+    # Fix random seeds and threads
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+
+    # Create logdir name
+    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+        os.path.basename(globals().get("__file__", "notebook")),
+        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+    ))
+
+    # Load data
+    mnist = MNIST()
+
+    # Create the TensorBoard writer
+    writer = tf.summary.create_file_writer(args.logdir, flush_millis=10*1000)
+
+    # Create the model
+    model = Model(args)
+
+    for epoch in range(args.epochs):
+        # (sgd_backpropagation): Run the `train_epoch` with `mnist.train` dataset
+        model.train_epoch(mnist.train)
+
+        # (sgd_backpropagation): Evaluate the dev data using `evaluate` on `mnist.dev` dataset
+        accuracy = model.evaluate(mnist.dev)
+        print("Dev accuracy after epoch {} is {:.2f}".format(epoch + 1, 100 * accuracy), flush=True)
+        with writer.as_default(step=epoch + 1):
+            tf.summary.scalar("dev/accuracy", 100 * accuracy)
+
+    # (sgd_backpropagation): Evaluate the test data using `evaluate` on `mnist.test` dataset
+    accuracy = model.evaluate(mnist.test)
+    print("Test accuracy after epoch {} is {:.2f}".format(epoch + 1, 100 * accuracy), flush=True)
+    with writer.as_default(step=epoch + 1):
+        tf.summary.scalar("test/accuracy", 100 * accuracy)
+
+    # Return test accuracy for ReCodEx to validate
+    return accuracy
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
