229691e9df5d854123c61364293f1b0c31329f07
Janek Putz <janekputz@web.de>
2022-03-20 22:21:13 +0100

    lab4: cifar_competition.py final

--- 04_cnns/cifar_competition.py
+++ 04_cnns/cifar_competition.py
@@ -1,39 +1,146 @@
 #!/usr/bin/env python3
-# TEAM MEMBERS:
+
+#CONTRIBUTORS
 # Antonio Krizmanic - 2b193238-8e3c-11ec-986f-f39926f24a9c
 # Janek Putz - e31a3cae-8e6c-11ec-986f-f39926f24a9c
 
+#All of the values used in the creation of the model are set as default in the .add_argument() methods or stated afterwards.
+#The .txt file was created in a seperate .py file locally
 import argparse
 import datetime
 import os
 import re
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
-
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") 
 import numpy as np
 import tensorflow as tf
-
+import tensorflow_addons as tfa
 from cifar10 import CIFAR10
-
-# : Define reasonable defaults and optionally more parameters
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=256, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=15, type=int, help="Number of epochs.")
+
+parser.add_argument("--batch_size", default=64, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=142, type=int, help="Number of epochs.")
+parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--momentum", default=0.9, type=float)
+parser.add_argument("--learning", default=0.001, type=float)
+parser.add_argument("--location", default = "cifar10_competition.h5", type = str)
 
-parser.add_argument("--conv_layers", default=3, type=int, help="Number of conv layers.")
-parser.add_argument("--filters", default=64, type=int, help="Number of filters/channels to use in one conv layer.")
-parser.add_argument("--kernel_size", default=3, type=int, help="Kernel size.")
-parser.add_argument("--stride", default=1, type=int, help="Stride in conv layers.")
-parser.add_argument("--padding", default="valid", type=str, help="Padding in conv layers.")
-parser.add_argument("--pooling", default="max", type=str, help="Type of pooling after each conv layer.")
-parser.add_argument("--pooling_size", default=2, type=int, help="Size of downscaling in pooling layer")
-parser.add_argument("--pooling_strides", default=2, type=int, help="Stride in pooling layer (usually == pooling_size")
-parser.add_argument("--final_avg_pooling", default=True, type=bool, help="Flag to use average pooling instead of dense layers.")
-parser.add_argument("--dense_layers", default=2, type=int, help="Number of dense layers.")
-parser.add_argument("--dense_size", default=128, type=int, help="Size of dense layers.")
-parser.add_argument("--dropout", default=0, type=float, help="Dropout rate for dropout layers after dense layers.")
+class Model(tf.keras.Model):
+    def __init__(self, struct: str, args: argparse.Namespace) -> None:
+        input = tf.keras.layers.Input(shape=[CIFAR10.H, CIFAR10.W, CIFAR10.C])
+        hidden = self.createTheModel(struct, input)
+        output = tf.keras.layers.Dense(10, activation=tf.nn.softmax)(hidden)
+        super().__init__(inputs=input, outputs=output)
+        
+        self.tb_callback = tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1)
+    
+    def conv_layer(self, shape: str, previous_layer):
+        filt = int(shape.split("-")[1])
+        ks = int(shape.split("-")[2])
+        stride = int(shape.split("-")[3])
+        pad = shape.split("-")[4]
+        return tf.keras.layers.Conv2D(filters = filt, kernel_size = ks,  strides=(stride,stride), padding = pad, activation=tf.nn.relu)(previous_layer)
 
+    def clear_BN(self, previous_layer):
+        return tf.keras.layers.BatchNormalization()(previous_layer)
+
+    def batch_normalisation(self, shape: str, previous_layer):
+        filt = int(shape.split("-")[1])
+        ks = int(shape.split("-")[2])
+        stride = int(shape.split("-")[3])
+        pad = shape.split("-")[4]
+        tempBN = tf.keras.layers.Conv2D(filters = filt, kernel_size = ks,  strides=(stride,stride), padding = pad, use_bias = None, activation=None)(previous_layer)
+        tempBN = tf.keras.layers.BatchNormalization()(tempBN)
+        return tf.keras.layers.ReLU()(tempBN)
+
+    def max_pool(self, shape: str, previous_layer):
+        pools = int(shape.split("-")[1])
+        stride = int(shape.split("-")[2])
+        return tf.keras.layers.MaxPool2D(pool_size= (pools,pools), strides = (stride,stride))(previous_layer)
+
+    def avg_pool(self, shape: str, previous_layer):
+        pools = int(shape.split("-")[1])
+        stride = int(shape.split("-")[2])
+        return tf.keras.layers.AveragePooling2D(pool_size= (pools,pools), strides = (stride,stride))(previous_layer)
+
+    def flat(self, previous_layer):
+        return tf.keras.layers.Flatten()(previous_layer)
+
+    def drop(self, shape: str, previous_layer):
+        dropout_rate = float(shape.split("-")[1])
+        return tf.keras.layers.Dropout(rate = dropout_rate)(previous_layer)
+
+    def hide(self, shape: str, previous_layer):
+        hls = int(shape.split("-")[1])
+        return tf.keras.layers.Dense(units = hls, activation = tf.nn.relu)(previous_layer) 
+
+    def spatial(self, shape: str, previous_layer):
+        rate_drop = float(shape.split("-")[1])
+        return tf.keras.layers.SpatialDropout2D(rate=rate_drop)(previous_layer) 
+
+    def residual_layer(self, shape: str, previous_layer):
+        preserve = previous_layer
+        tempRL = previous_layer
+        print(f'{shape}, buraz')
+        sequence_of_actions = shape.replace("R-[", "").replace("]", "").split(",")
+        print(sequence_of_actions)
+        for action in sequence_of_actions:
+            if action.split("-")[0] == "C":
+                tempRL = self.conv_layer(action,tempRL)
+            elif action.split("-")[0] == "CB":
+                tempRL = self.batch_normalisation(action,tempRL)
+            else: 
+                print("nema smisla staviti taj sloj u reg")
+        return tf.keras.layers.Add()([preserve, tempRL])
+
+    def ResNetBottleneck(self, shape: str, previous_layer):
+        size = int(shape.split("-")[1])
+        preserve = previous_layer
+        tempRNB = previous_layer
+        tempRNB = tf.keras.layers.Conv2D(filters = size/2, kernel_size = 1,  strides=1, padding = "same", activation=None, use_bias = None)(tempRNB)
+        tempRNB = tf.keras.layers.BatchNormalization()(tempRNB)
+        tempRNB = tf.keras.layers.ReLU()(tempRNB)
+        tempRNB = tf.keras.layers.Conv2D(filters = size/2, kernel_size = 3,  strides=1, padding = "same", activation=None, use_bias = None)(tempRNB)
+        tempRNB = tf.keras.layers.BatchNormalization()(tempRNB)
+        tempRNB = tf.keras.layers.ReLU()(tempRNB)
+        tempRNB = tf.keras.layers.Conv2D(filters = size, kernel_size = 1,  strides=1, padding = "same", activation=None, use_bias = None)(tempRNB)
+        tempRNB = tf.keras.layers.BatchNormalization()(tempRNB)
+        return tf.keras.layers.ReLU()(tf.keras.layers.Add()([preserve, tempRNB]))
+
+    def createTheModel(self, structure: str, input_layer):
+        hidden = input_layer
+        model_list = structure.split(",")
+        for layer in model_list:
+            if layer.split("-")[0] == "C":
+                hidden = self.conv_layer(layer,hidden)
+            elif layer.split("-")[0] == "CB" and layer[-1] != "]":
+                hidden = self.batch_normalisation(layer,hidden)
+            elif layer.split("-")[0] == "M":
+                hidden = self.max_pool(layer,hidden)
+            elif layer.split("-")[0] == "R":
+                strr = layer
+                for other_half in model_list:
+                    if other_half.split("-")[0] == "CB" and other_half[-1] == "]":
+                        strr = strr +","+ other_half
+                hidden = self.residual_layer(strr,hidden)
+            elif layer.split("-")[0] == "F":
+                hidden = self.flat(hidden)
+            elif layer.split("-")[0] == "D":
+                hidden = self.drop(layer,hidden)
+            elif layer.split("-")[0] == "H":
+                hidden = self.hide(layer,hidden)
+            elif layer.split("-")[0] == "SD":
+                hidden = self.spatial(layer,hidden)
+            elif layer.split("-")[0] == "A":
+                hidden = self.avg_pool(layer,hidden)
+            elif layer.split("-")[0] == "RNB":
+                hidden = self.ResNetBottleneck(layer,hidden)
+            elif layer.split("-")[0] == "CBN":
+                hidden = self.clear_BN(hidden)
+            else:  
+                continue
+        return hidden
 
 def main(args: argparse.Namespace) -> None:
     # Fix random seeds and threads
@@ -51,66 +158,45 @@ def main(args: argparse.Namespace) -> None:
     # Load data
     cifar = CIFAR10()
 
-    # findings: (IMPORTANT: can change with other changes in architecture)
-    # - batch size 256 works the best
-    # - dropout in dense layers does not improve
-    # - doubling channels step by step after max pooling layers does not improve
-    # - 3 conv layers are not deep enough to use global average pooling instead of dense layers
-    # TODO: try approaches from slide 3 and 11 from slide set 2 about CNNs + regularization methods
-
-    # : Create the model and train it
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Input(shape=[CIFAR10.H, CIFAR10.W, CIFAR10.C]))
-    # conv layers
-    max_pool_mul = 1
-    for i in range(args.conv_layers):
-        model.add(tf.keras.layers.Conv2D(filters=args.filters*max_pool_mul, kernel_size=args.kernel_size, strides=args.stride,
-                                         padding=args.padding, activation=tf.nn.relu))
-        # add max pooling only if pooling is active. If average pooling is applied after the conv blocks instead of
-        # dense layers dont add last max pooling layer
-        if args.pooling and not (args.final_avg_pooling and i != args.conv_layers - 1):
-            pool_class = tf.keras.layers.MaxPooling2D if args.pooling == 'max' else tf.keras.layers.AveragePooling2D
-            model.add(pool_class(pool_size=args.pooling_size, strides=args.pooling_strides))
-            max_pool_mul *= 2
-
-    # average pooling OR dense layers
-    if not args.final_avg_pooling:
-        model.add(tf.keras.layers.Flatten())
-        for i in range(args.dense_layers):
-            model.add(tf.keras.layers.Dense(args.dense_size, activation=tf.nn.relu))
-            model.add(tf.keras.layers.Dropout(args.dropout))
-    else:
-        model.add(tf.keras.layers.AveragePooling2D(pool_size=args.pooling_size, strides=args.pooling_strides))
-        model.add(tf.keras.layers.Flatten())
-
-    # final output layer
-    model.add(tf.keras.layers.Dense(CIFAR10.LABELS, activation=tf.nn.softmax))
-
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-        metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")]
+    train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range = 15,
+        zoom_range = 0.15,
+        width_shift_range = 0.1,
+        height_shift_range = 0.1,
+        horizontal_flip = True
     )
+
+    model = Model("C-32-3-1-same,CBN,RNB-32,M-2-2,SD-0.15,C-64-3-1-same,CBN,RNB-64,M-2-2,SD-0.15,C-128-3-1-same,CBN,RNB-128,M-2-2,C-256-3-1-same,CBN,RNB-256,SD-0.15,F,H-64,H-64", args)
     model.summary()
-
-    # model = tf.keras.models.load_model("C:\\Users\\janek\\Development\\Git\\Prag\\deep-learning-lecture\\04_cnns\\logs\\cifar_competition.py-2022-03-14_160914-bs=256,cl=3,dl=2,ds=128,d=0,e=10,f=64,ks=3,p=valid,p=max,ps=2,ps=2,s=42,s=1,t=1\\cifar_comp_model.h5")
-
+    model.compile(
+        optimizer=tf.keras.optimizers.SGD(learning_rate = args.learning,momentum = args.momentum),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy("accuracy")]
+        )
     history = model.fit(
-        cifar.train.data["images"], cifar.train.data["labels"],
-        batch_size=args.batch_size, epochs=args.epochs,
+        train_generator.flow(x = cifar.train.data["images"], y=cifar.train.data["labels"], batch_size=args.batch_size, seed=args.seed),
+        shuffle=False, epochs=args.epochs,
         validation_data=(cifar.dev.data["images"], cifar.dev.data["labels"]),
-        callbacks=[tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1, update_freq=100, profile_batch=0)],
+        callbacks=[tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1)],
+        verbose = 2
     )
 
-    model.save(os.path.join(args.logdir, 'cifar_comp_model.h5'), include_optimizer=True)
-
+    model.save(os.path.join(args.logdir, args.location), include_optimizer=True)
+    
+    model = tf.keras.models.load_model('models\cifar10_competition.h5')
+    predictions = model.predict(cifar.test.data['images'])
+    predictions = tf.argmax(predictions, axis = -1)
+    
     # Generate test set annotations, but in args.logdir to allow parallel execution.
     os.makedirs(args.logdir, exist_ok=True)
-    with open(os.path.join(args.logdir, "cifar_competition_test.txt"), "w", encoding="utf-8") as predictions_file:
-        for probs in model.predict(cifar.test.data["images"], batch_size=args.batch_size):
-            print(np.argmax(probs), file=predictions_file)
-
+    with open("cifar10_test.txt", "w", encoding="utf-8") as predictions_file:
+        data = tf.data.Dataset.from_tensor_slices(predictions)
+        for i in data:
+                predictions_file.write(str(np.asarray(i)) + "\n")
+    predictions_file.close()
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
     main(args)
+
+
