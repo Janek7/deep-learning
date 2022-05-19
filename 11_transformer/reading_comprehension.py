a14c8628893326971d2f6409630e8ccc0782050d
Janek Putz <janekputz@web.de>
2022-05-19 14:37:16 +0200

    lab11: reading_comprehension.py prepare datasets

--- 11_transformer/reading_comprehension.py
+++ 11_transformer/reading_comprehension.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# TEAM MEMBERS:
+# Antonio Krizmanic - 2b193238-8e3c-11ec-986f-f39926f24a9c
+# Janek Putz - e31a3cae-8e6c-11ec-986f-f39926f24a9c
 import argparse
 import datetime
 import os
@@ -11,13 +14,19 @@ import transformers
 
 from reading_comprehension_dataset import ReadingComprehensionDataset
 
-# TODO: Define reasonable defaults and optionally more parameters.
+# : Define reasonable defaults and optionally more parameters.
 # Also, you can set the number of the threads 0 to use all your CPU cores.
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
+parser.add_argument("--batch_size", default=8, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=4, type=int, help="Number of epochs.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+# additional parameter
+parser.add_argument("--decay", default="None", type=str, help="Learning decay rate type")
+parser.add_argument("--learning_rate", default=2e-5, type=float, help="Initial learning rate.")
+parser.add_argument("--learning_rate_final", default=1e-5, type=float, help="Final learning rate.")
+parser.add_argument("--label_smoothing", default=0.1, type=float, help="Label smoothing.")
+parser.add_argument("--warmup_epochs", default=1, type=float, help="Number of warmup epochs.")
 
 
 def main(args: argparse.Namespace) -> None:
@@ -35,11 +44,71 @@ def main(args: argparse.Namespace) -> None:
 
     # Load the Electra Czech small lowercased
     tokenizer = transformers.AutoTokenizer.from_pretrained("ufal/robeczech-base")
-    robeczech = transformers.TFAutoModel.from_pretrained("ufal/robeczech-base")
+    # robeczech = transformers.TFAutoModel.from_pretrained("ufal/robeczech-base")
 
     # Load the data.
-    dataset = ReadingComprehensionDataset()
-    exit()
+    reading_comprehension_dataset = ReadingComprehensionDataset()
+    debug = False
+
+    def create_dataset(name) -> tf.data.Dataset:
+        dataset = getattr(reading_comprehension_dataset, name)
+
+        # 1) tokenize data
+        context_question_pairs, answer_starts, answer_ends = [], [], []
+        for paragraph in dataset.paragraphs:  # iterate over all paragraphs
+            for qa_pair in paragraph["qas"]:  # iterate over all questions in a paragraph
+                encoded_context_question_pair = tokenizer(paragraph["context"], qa_pair["question"], max_length=512,
+                                                          truncation="only_first", padding="max_length")
+                # if dataset is test, append only to context_question_pairs, answers are empty
+                if name == 'test':
+                    context_question_pairs.append(encoded_context_question_pair["input_ids"])
+                else:
+                    for answer in qa_pair["answers"]:  # iterate over all answers of a question
+                        # compute answer end index
+                        last_answer_word = answer["text"].split(" ")[-1]
+                        answer_end_idx = paragraph["context"][answer["start"]:].find(last_answer_word)
+                        answer_end_idx += answer["start"]
+                        answer_end_token_idx = encoded_context_question_pair.char_to_token(answer["start"])
+                        if debug:
+                            print(paragraph["context"])
+                            print(encoded_context_question_pair["input_ids"])
+                            print(answer["text"], tokenizer(answer["text"])["input_ids"])
+                            print("start idx", answer["start"], "token:", encoded_context_question_pair.char_to_token(answer["start"]))
+                            print(last_answer_word)
+                            print("end idx", answer_end_idx, "token:", encoded_context_question_pair.char_to_token(answer_end_idx))
+                            print("-------------")
+
+                        # only if computation was successful, append whole triple (fails in 22 cases for train)
+                        if answer_end_token_idx is not None:
+                            # A) record for every answer the same tokenized context/question
+                            context_question_pairs.append(encoded_context_question_pair["input_ids"])
+                            # B) record for every answer the respective token IDs of start and end words
+                            answer_starts.append(encoded_context_question_pair.char_to_token(answer["start"]))
+                            answer_ends.append(answer_end_token_idx)
+        # convert lists to tensors
+        context_question_pairs = tf.constant(context_question_pairs)
+        answer_starts = tf.constant(answer_starts)
+        answer_ends = tf.constant(answer_ends)
+
+        # 2) create tf.data.DataSet
+        if name != 'test':
+            dataset = tf.data.Dataset.from_tensor_slices((context_question_pairs, answer_starts, answer_ends))
+            # combine answer_start and answer_end to one output dict per sample
+            def one_output_map(context_question_pair, answer_start, answer_end):
+                return context_question_pair, {"answer_start": answer_start, "answer_end": answer_end}
+            dataset = dataset.map(one_output_map)
+        else:
+            dataset = tf.data.Dataset.from_tensor_slices((context_question_pairs))
+        if name == "train":
+            dataset = dataset.shuffle(buffer_size=10000, seed=args.seed)
+        print(f"{name} dataset: {len(dataset)}")
+        dataset = dataset.batch(args.batch_size)
+        return dataset
+
+    print("create datasets")
+    train = create_dataset("train")
+    dev = create_dataset("dev")
+    test = create_dataset("test")
 
     # TODO: Create the model and train it
     model = ...
